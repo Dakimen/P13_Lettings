b5b4bfcf0751e41092521a003aa07f862885fda5
Dmitry Akimenko <dakimenko@yahoo.com>
2026-01-29 19:36:20 +0300

    Modified allowed hosts

--- oc_lettings_site/settings.py
+++ oc_lettings_site/settings.py
@@ -40,7 +40,10 @@ SECRET_KEY = os.getenv("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = ["localhost", "127.0.0.1"]
+ALLOWED_HOSTS = [
+    "localhost", "127.0.0.1",
+    ".containers.yandexcloud.net",
+    ]
 
 
 # Application definition
